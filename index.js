c31469debaa8b5379b9fbea6d9dc440bc0ee4736
psarka1996 <pankaj.soni@arkasoftwares.com>
2022-12-17 15:29:05 +0530

    aalask

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app=express();
 const PORT=8000;
 
 app.get("/",(req,res)=>{
-    res.send("<h1>hello Fullstack developers</h1>")
+    res.send("<h1>hello Aaakash developers</h1>")
 })
 
 app.listen(PORT,(req,res)=>{
